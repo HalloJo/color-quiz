1189d095a943be7e116993f4064c3c34a97d8bc7
Jorik van Ruiswijk <jorik.van.ruiswijk@infoprojects.nl>
2023-06-22 17:09:06 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import "./App.scss";
+
+const App = () => {
+  const [color, setColor] = useState<string>("");
+  const [answers, setAnswers] = useState<string[]>([]);
+  const [wrong, setWrong] = useState<boolean>(false);
+
+  const getRandomColor = () => {
+    const digits = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+    ];
+
+    const color = new Array(6)
+      .fill("")
+      .map(() => digits[Math.floor(Math.random() * digits.length)])
+      .join("");
+
+    return `#${color}`;
+  };
+
+  const generateColors = () => {
+    const correctAnswer = getRandomColor();
+
+    setColor(correctAnswer);
+    setAnswers(
+      [correctAnswer, getRandomColor(), getRandomColor()].sort(
+        () => 0.5 - Math.random()
+      )
+    );
+  };
+
+  const handleAnswerClicked = (answer: string) => {
+    if (answer === color) {
+      // correct
+      setWrong(false);
+      generateColors();
+    } else {
+      // wrong
+      setWrong(true);
+    }
+  };
+
+  useEffect(() => {
+    generateColors();
+  }, []);
+
+  return (
+    <>
+      {wrong ? <h2>❌ Wrong!</h2> : <h2>⁉️ Color Quiz</h2>}
+      <div className="square" style={{ background: color }}>
+        <p>Guess me.</p>
+      </div>
+      <div className="answers">
+        {answers.map((answer) => (
+          <button key={answer} onClick={() => handleAnswerClicked(answer)}>
+            {answer}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default App;
