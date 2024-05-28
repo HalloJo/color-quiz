bd76471b6ac96b94402e04dbb1214747d6e6ebd8
Jorik van Ruiswijk <jorikvanruiswijk@gmail.com>
2024-05-28 20:35:47 +0200

    Extract getRandomColor to seperate function

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,12 @@
 import { useEffect, useState } from "react";
 import "./App.scss";
+import { getRandomColor } from "./utils/get-random-color";
 
 const App = () => {
   const [color, setColor] = useState<string>("");
   const [answers, setAnswers] = useState<string[]>([]);
   const [wrong, setWrong] = useState<boolean>(false);
   const [correct, setCorrect] = useState<boolean>(false);
-
-  const getRandomColor = () => {
-    const digits = [
-      "0",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-    ];
-
-    const color = new Array(6)
-      .fill("")
-      .map(() => digits[Math.floor(Math.random() * digits.length)])
-      .join("");
-
-    return `#${color}`;
-  };
 
   const generateColors = () => {
     const correctAnswer = getRandomColor();
